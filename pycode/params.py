6e706d7063fa9ceee87857c43ec95ee9dafc0929
Vedad Hodzic <vedadh@ulrik.uio.no>
2015-03-15 18:58:34 +0100

    Added more constants for milestone 2.

--- pycode/params.py
+++ pycode/params.py
@@ -6,6 +6,13 @@ Mpc				  = 3.08568025e22			# m
 # Physical constants
 c				  = 2.99792458e8			# m s-1
 G				  = 6.67258e-11				# m3 kg-1 s-2
+epsilon_0		  = 13.605698				# Ionization energy for H I
+m_e				  = 9.10938188e-31			# Mass of electron [kg]
+m_H				  = 1.673534e-27			# Mass of H [kg]
+sigma_T			  =	6.652462e-29			# Thomson cross-section [m2]
+alpha			  = 7.29735308e-3			# Not sure what this is. Ask.
+hbar			  = 1.05457148e-34			# Planck constant [Js]
+k_b				  = 1.3806503e-23			# Boltzmann constant [J/K]
 
 # Cosmological parameters
 Omega_b			  = 0.046
@@ -16,5 +23,9 @@ Omega_lambda	  = 1. - Omega_m - Omega_b - Omega_r - Omega_nu
 h0				  = 0.7
 H_0				  = h0 * 100 * 1e3 / Mpc
 rho_c0			  = 3 * H_0*H_0 / (8 * np.pi * G)
+T_0				  = 2.725					# K
+n_s				  = 1.0						# Spectral index (ask about this)
+A_s				  =	1.0						# Don't know what this is. Ask
+
 
 
